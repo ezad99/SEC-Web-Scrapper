d2089572ea9bb3757be9219e3fe7e659582ade0e
Ezad99 <ezadhaziq99@gmail.com>
2024-05-07 16:55:59 +0100

    feat: Changed the base_query to fit with filterApi

--- myenv/filing_downloader.py
+++ myenv/filing_downloader.py
@@ -6,66 +6,54 @@ renderApi = RenderApi(api_key=API_KEY)
 queryApi = QueryApi(api_key=API_KEY)
 filterApi = FullTextSearchApi(api_key=API_KEY)
 
+# base_query = {
+#   "query": { 
+#       "query_string": { 
+#           "query": "PLACEHOLDER", # this will be set during runtime 
+#           "time_zone": "America/New_York"
+#       } 
+#   },
+#   "from": "0",
+#   "size": "200", # dont change this
+#   # sort returned filings by the filedAt key/value
+#   "sort": [{ "filedAt": { "order": "desc" } }]
+# }
+
+filterVariables = "cybersecurity"
+
 base_query = {
-  "query": { 
-      "query_string": { 
-          "query": "PLACEHOLDER", # this will be set during runtime 
-          "time_zone": "America/New_York"
-      } 
-  },
-  "from": "0",
-  "size": "200", # dont change this
-  # sort returned filings by the filedAt key/value
-  "sort": [{ "filedAt": { "order": "desc" } }]
+    "query": filterVariables,
+    "formTypes": ["10-K", "10-KT", "10KSB"],
+    "startDate": "2001-01-01", # Gets all filings from 2001 
+    "endDate": "2022-12-31", # Until the end of 2022
+    "page": 0
 }
+
 
 # open the file we use to store the filing URLs
 log_file = open("filing_urls.txt", "a")
 
-# start with filings filed in 2022, then 2020, 2019, ... up to 2001
-# gets all filings from 2001 - 2020
-for year in range(2022, 2001, -1):
-  print("Starting download for year {year}".format(year=year))
-  
-  # a single search universe is represented as a month of the given year
-  for month in range(1, 13, 1):
-    # get 10-Q and 10-Q/A filings filed in year and month
-    # resulting query example: "formType:\"10-Q\" AND filedAt:[2021-01-01 TO 2021-01-31]"
-    universe_query = \
-        "formType:(\"10-K\", \"10-KT\", \"10KSB\", \"10KT405\", \"10KSB40\", \"10-K405\") AND " + \
-        "filedAt:[{year}-{month:02d}-01 TO {year}-{month:02d}-31]" \
-        .format(year=year, month=month)
-  
-    # set new query universe for year-month combination
-    base_query["query"]["query_string"]["query"] = universe_query;
+for page in range(0, 9900, 100): # Query API returns a max of 100 fillings per search
+    # set new "from" starting position of search 
+    base_query["page"] = page;
 
-    # paginate through results by increasing "from" parameter 
-    # until we don't find any matches anymore
-    # uncomment next line to fetch all 10,000 filings
-    # for from_batch in range(0, 9800, 200): 
-    for from_batch in range(0, 400, 200): # Query API returns a max of 200 fillings per search
-      # set new "from" starting position of search 
-      base_query["from"] = from_batch;
+    # gets the filings from the Api
+    response = filterApi.get_filings(base_query)
 
-      # gets the filings from the Api
-      response = queryApi.get_filings(base_query)
-
-      # no more filings in search universe
-      if len(response["filings"]) == 0:
+    # no more filings in search universe
+    if len(response["filings"]) == 0:
         break;
 
-      # for each filing, only save the URL pointing to the filing itself 
-      # and ignore all other data. 
-      # the URL is set in the dict key "linkToFilingDetails"
-      urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
+    # for each filing, only save the URL pointing to the filing itself 
+    # and ignore all other data. 
+    # the URL is set in the dict key "linkToFilingDetails"
+    urls_list = list(map(lambda x: x["linkToFilingDetails"], response["filings"]))
 
-      # transform list of URLs into one string by joining all list elements
-      # and add a new-line character between each element.
-      urls_string = "\n".join(urls_list) + "\n"
-      
-      log_file.write(urls_string)
-
-    print("Filing URLs downloaded for {year}-{month:02d}".format(year=year, month=month))
+    # transform list of URLs into one string by joining all list elements
+    # and add a new-line character between each element.
+    urls_string = "\n".join(urls_list) + "\n"
+    
+    log_file.write(urls_string)
 
 log_file.close()
 
